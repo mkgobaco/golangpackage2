27d72184cb655a383b0b0e136f388c11048739c1
Michael Gobaco <michael.gobaco@pluto.tv>
2023-05-26 15:02:02 -0700

    Add call to package1.PackageName() in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
+import "github.com/mkgobaco/golangpackage1/pkg/package1"
+
 func main() {
 	println("Hello, World!")
-	println(golangpackage2.PackageName())
+	println(package1.PackageName())
 }
